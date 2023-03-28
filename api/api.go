d2d897658993c957724269a50a0d756a0c099508
siwonpada <psw041115@gmail.com>
2023-03-28 21:35:09 +0900

    use folder structor

--- api/api.go
+++ api/api.go
@@ -1,1 +1,31 @@
 package api
+
+import (
+	"net/http"
+	"encoding/json"
+)
+
+type User struct {
+	Nickname string `json:"nickname"`
+	Email    string `json:"email"`
+}
+
+var users = map[string]*User{}
+
+func MainPageHander (w http.ResponseWriter, r * http.Request) {
+	w.Write([]byte("Hello World!"))
+}
+
+func UserHander (w http.ResponseWriter, r * http.Request) {
+	switch r.Method {
+	case http.MethodGet:
+		json.NewEncoder(w).Encode(users)
+	case http.MethodPost:
+		var user User
+		json.NewDecoder(r.Body).Decode(&user)
+
+		users[user.Email] = &user
+
+		json.NewEncoder(w).Encode(user)
+	}
+}
--- main.go
+++ main.go
@@ -1,14 +1,9 @@
 package main
 
 import (
-	"encoding/json"
+	"calendar/api"
 	"net/http"
 )
-
-type User struct {
-	Nickname string `json:"nickname"`
-	Email    string `json:"email"`
-}
 
 func jsonContentTypeMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
@@ -18,28 +13,11 @@ func jsonContentTypeMiddleware(next http.Handler) http.Handler {
 	})
 }
 
-var users = map[string]*User{}
-
 func main() {
 	mux := http.NewServeMux()
 
-	mainpage := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Hello World!"))
-	})
-
-	userHander := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		switch r.Method {
-		case http.MethodGet:
-			json.NewEncoder(w).Encode(users)
-		case http.MethodPost:
-			var user User
-			json.NewDecoder(r.Body).Decode(&user)
-
-			users[user.Email] = &user
-
-			json.NewEncoder(w).Encode(user)
-		}
-	})
+	mainpage := http.HandlerFunc(api.MainPageHander)
+	userHander := http.HandlerFunc(api.UserHander)
 
 	mux.Handle("/", mainpage);
 	mux.Handle("/users", jsonContentTypeMiddleware(userHander));
