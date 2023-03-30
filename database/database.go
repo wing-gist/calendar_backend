1947c14f6fc7c33b5407fc0196285874869a958f
siwonpada <psw041115@gmail.com>
2023-03-31 02:56:05 +0900

    database_conn

--- database/database.go
+++ database/database.go
@@ -1,1 +1,38 @@
 package database
+
+import (
+	"context"
+	"log"
+	"os"
+	"time"
+
+	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+type User struct {
+	Nickname string `bson:"nickname"`
+	Email    string `bson:"email"`
+}
+
+func Connect() (*mongo.Client, context.Context, context.CancelFunc) {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	client, err := mongo.NewClient(options.Client().ApplyURI(os.Getenv("MONGO_DB_CONNECTION")))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+
+	err = client.Connect(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return client, ctx, cancel
+}
