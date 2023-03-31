1f9edf1b8667fb429014e7b845dda173285a0d3f
siwonpada <psw041115@gmail.com>
2023-03-31 15:58:02 +0900

    connect go and mongo

--- api/api.go
+++ api/api.go
@@ -1,8 +1,12 @@
 package api
 
 import (
-	"net/http"
 	"encoding/json"
+	"net/http"
+
+	"calendar/api/database"
+
+	"go.mongodb.org/mongo-driver/bson"
 )
 
 type User struct {
@@ -10,22 +14,41 @@ type User struct {
 	Email    string `json:"email"`
 }
 
-var users = map[string]*User{}
-
-func MainPageHander (w http.ResponseWriter, r * http.Request) {
+func MainPageHander(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello World!"))
 }
 
-func UserHander (w http.ResponseWriter, r * http.Request) {
+func UserHander(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
 	case http.MethodGet:
+		client, ctx, cancel := database.Connect()
+		defer cancel()
+
+		Cursor, err := database.Find(client, "users", bson.D{{}}, ctx)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+
+		var users = []*User{}
+		for Cursor.Next(ctx) {
+			var user User
+			Cursor.Decode(&user)
+			users = append(users, &user)
+		}
 		json.NewEncoder(w).Encode(users)
 	case http.MethodPost:
 		var user User
 		json.NewDecoder(r.Body).Decode(&user)
+		client, ctx, cancel := database.Connect()
+		defer cancel()
 
-		users[user.Email] = &user
+		InsertOneResult, err := database.InsertOne(client, "users", bson.D{{"Nickname", user.Nickname}, {"Email", user.Email}}, ctx)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
 
-		json.NewEncoder(w).Encode(user)
+		json.NewEncoder(w).Encode(InsertOneResult)
 	}
 }
