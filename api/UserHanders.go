a16716143f2f2fe3e2e2c9ca6ab11b467727bfbd
siwonpada <psw041115@gmail.com>
2023-04-24 00:03:47 +0900

    Revert "validate JWT"
    This reverts commit f783741a46265864036f577853901cc5349c0824.

--- api/UserHanders.go
+++ api/UserHanders.go
@@ -4,9 +4,13 @@ import (
 	"context"
 	"encoding/json"
 	"net/http"
+	"os"
+	"time"
 
 	"calendar/database"
 
+	"github.com/dgrijalva/jwt-go/v4"
+	"github.com/google/uuid"
 	"go.mongodb.org/mongo-driver/bson"
 	"golang.org/x/crypto/bcrypt"
 )
@@ -46,4 +50,63 @@ func UserPostHandler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	json.NewEncoder(w).Encode(InsertOneResult)
+}
+
+func ValidateUser(w http.ResponseWriter, r *http.Request) {
+	var user User
+	json.NewDecoder(r.Body).Decode(&user)
+
+	filter := bson.D{{"email", user.Email}}
+
+	SingleResult := database.FindOne("users", filter)
+
+	var UserFromDB User
+	SingleResult.Decode(&UserFromDB)
+
+	err := bcrypt.CompareHashAndPassword(UserFromDB.Passsword, user.Passsword)
+	if err != nil {
+		w.WriteHeader(http.StatusUnauthorized)
+		return
+	}
+
+	preAuthToken := AuthtokenClaims{
+		TokenUUID: uuid.NewString(),
+		UserID:    UserFromDB.ID.String(),
+		Nickname:  UserFromDB.Nickname,
+		StandardClaims: jwt.StandardClaims{
+			ExpiresAt: jwt.At(time.Now().Add(time.Minute * 15)),
+		},
+	}
+
+	authToken := jwt.NewWithClaims(jwt.SigningMethodHS256, preAuthToken)
+	signedAuthToken, err := authToken.SignedString([]byte(os.Getenv("JWT_SECRET")))
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	json.NewEncoder(w).Encode(signedAuthToken)
+}
+
+func ValidateJWT(w http.ResponseWriter, r *http.Request) {
+	cookie, err := r.Cookie("calendar_JWT")
+	if err != nil {
+		w.WriteHeader(http.StatusUnauthorized)
+		return
+	}
+
+	token, err := jwt.ParseWithClaims(cookie.Value, &AuthtokenClaims{}, func(token *jwt.Token) (interface{}, error) {
+		return []byte(os.Getenv("JWT_SECRET")), nil
+	})
+	if err != nil {
+		w.WriteHeader(http.StatusUnauthorized)
+		return
+	}
+
+	if token.Valid {
+		json.NewEncoder(w).Encode(token.Claims)
+	} else {
+		w.WriteHeader(http.StatusUnauthorized)
+		return
+	}
 }
