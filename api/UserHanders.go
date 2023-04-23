75046cb2a3a92166a54ab4773b036663534972a2
siwonpada <psw041115@gmail.com>
2023-04-23 21:42:01 +0900

    auth

--- api/UserHanders.go
+++ api/UserHanders.go
@@ -4,13 +4,9 @@ import (
 	"context"
 	"encoding/json"
 	"net/http"
-	"os"
-	"time"
 
 	"calendar/database"
 
-	"github.com/dgrijalva/jwt-go/v4"
-	"github.com/google/uuid"
 	"go.mongodb.org/mongo-driver/bson"
 	"golang.org/x/crypto/bcrypt"
 )
@@ -36,6 +32,10 @@ func UserGetHandler(w http.ResponseWriter, r *http.Request) {
 func UserPostHandler(w http.ResponseWriter, r *http.Request) {
 	var user User
 	json.NewDecoder(r.Body).Decode(&user)
+	if user.Email == "" || user.Nickname == "" || user.Passsword == nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
 
 	user.Passsword, _ = bcrypt.GenerateFromPassword(user.Passsword, bcrypt.DefaultCost)
 
@@ -46,65 +46,4 @@ func UserPostHandler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	json.NewEncoder(w).Encode(InsertOneResult)
-}
-
-func ValidateUser(w http.ResponseWriter, r *http.Request) {
-	var user User
-	json.NewDecoder(r.Body).Decode(&user)
-
-	filter := bson.D{{"email", user.Email}}
-
-	SingleResult := database.FindOne("users", filter)
-
-	var UserFromDB User
-	SingleResult.Decode(&UserFromDB)
-
-	err := bcrypt.CompareHashAndPassword(UserFromDB.Passsword, user.Passsword)
-	if err != nil {
-		w.WriteHeader(http.StatusUnauthorized)
-		return
-	}
-
-	preAuthToken := AuthtokenClaims{
-		TokenUUID: uuid.NewString(),
-		UserID:    UserFromDB.ID.String(),
-		Nickname:  UserFromDB.Nickname,
-		StandardClaims: jwt.StandardClaims{
-			ExpiresAt: jwt.At(time.Now().Add(time.Minute * 15)),
-		},
-	}
-
-	authToken := jwt.NewWithClaims(jwt.SigningMethodHS256, preAuthToken)
-	signedAuthToken, err := authToken.SignedString([]byte(os.Getenv("JWT_SECRET")))
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
-
-	cookie := http.Cookie{Name: "MyJWT", Value: signedAuthToken, Expires: time.Now().Add(time.Minute * 15), MaxAge: 0, Secure: false, HttpOnly: false}
-	http.SetCookie(w, &cookie)
-	w.Write([]byte(signedAuthToken))
-}
-
-func ValidateJWT(w http.ResponseWriter, r *http.Request) {
-	cookie, err := r.Cookie("MyJWT")
-	if err != nil {
-		w.WriteHeader(http.StatusUnauthorized)
-		return
-	}
-
-	token, err := jwt.ParseWithClaims(cookie.Value, &AuthtokenClaims{}, func(token *jwt.Token) (interface{}, error) {
-		return []byte(os.Getenv("JWT_SECRET")), nil
-	})
-	if err != nil {
-		w.WriteHeader(http.StatusUnauthorized)
-		return
-	}
-
-	if token.Valid {
-		json.NewEncoder(w).Encode(token.Claims)
-	} else {
-		w.WriteHeader(http.StatusUnauthorized)
-		return
-	}
 }
--- api/auth.go
+++ api/auth.go
@@ -1,7 +1,16 @@
 package api
 
 import (
+	"calendar/database"
+	"encoding/json"
+	"net/http"
+	"os"
+	"time"
+
 	"github.com/dgrijalva/jwt-go/v4"
+	"github.com/google/uuid"
+	"go.mongodb.org/mongo-driver/bson"
+	"golang.org/x/crypto/bcrypt"
 )
 
 type AuthtokenClaims struct {
@@ -9,4 +18,67 @@ type AuthtokenClaims struct {
 	UserID    string `json:"user_id"`
 	Nickname  string `json:"nickname"`
 	jwt.StandardClaims
+}
+
+func ValidateUser(w http.ResponseWriter, r *http.Request) {
+	var user User
+	json.NewDecoder(r.Body).Decode(&user)
+	if user.Email == "" || user.Passsword == nil {
+		w.WriteHeader(http.StatusBadRequest)
+	}
+
+	filter := bson.D{{"email", user.Email}}
+
+	SingleResult := database.FindOne("users", filter)
+
+	var UserFromDB User
+	SingleResult.Decode(&UserFromDB)
+
+	err := bcrypt.CompareHashAndPassword(UserFromDB.Passsword, user.Passsword)
+	if err != nil {
+		w.WriteHeader(http.StatusUnauthorized)
+		return
+	}
+
+	preAuthToken := AuthtokenClaims{
+		TokenUUID: uuid.NewString(),
+		UserID:    UserFromDB.ID.String(),
+		Nickname:  UserFromDB.Nickname,
+		StandardClaims: jwt.StandardClaims{
+			ExpiresAt: jwt.At(time.Now().Add(time.Minute * 15)),
+		},
+	}
+
+	authToken := jwt.NewWithClaims(jwt.SigningMethodHS256, preAuthToken)
+	signedAuthToken, err := authToken.SignedString([]byte(os.Getenv("JWT_SECRET")))
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	cookie := http.Cookie{Name: "calendar_JWT", Value: signedAuthToken, Expires: time.Now().Add(time.Minute * 15), MaxAge: 0, Secure: false, HttpOnly: false}
+	http.SetCookie(w, &cookie)
+}
+
+func ValidateJWT(w http.ResponseWriter, r *http.Request) {
+	cookie, err := r.Cookie("calendar_JWT")
+	if err != nil {
+		w.WriteHeader(http.StatusUnauthorized)
+		return
+	}
+
+	token, err := jwt.ParseWithClaims(cookie.Value, &AuthtokenClaims{}, func(token *jwt.Token) (interface{}, error) {
+		return []byte(os.Getenv("JWT_SECRET")), nil
+	})
+	if err != nil {
+		w.WriteHeader(http.StatusUnauthorized)
+		return
+	}
+
+	if token.Valid {
+		json.NewEncoder(w).Encode(token.Claims)
+	} else {
+		w.WriteHeader(http.StatusUnauthorized)
+		return
+	}
 }
